bc5baac97b3c3c24b84c7c44d62a47032157179a
flame78 <flame78@gmail.com>
2014-06-10 22:39:56 +0300

    Collision at 70%

--- AV/scripts/Game.js
+++ AV/scripts/Game.js
@@ -1,27 +1,28 @@
 ﻿window.onload = function () {
     var WIDTH = 320;
     var HEIGHT = 200;
-    var PLAYER_RADIUS =  HEIGHT / 20;
+    var КПД = 0.8;
+    var CIRCLE_RADIUS = HEIGHT / 20;
     var G_ACCELERATION_FOR_FRAME = 9.8 / 60;
     var RUN_ACCELERATION_FOR_FRAME = 4 / 60;
     var svgNS = 'http://www.w3.org/2000/svg';
     var ball = document.createElementNS(svgNS, 'circle');
-    ball.setAttribute('cx', WIDTH/4 * 3);
-    ball.setAttribute('cy', HEIGHT/10);
-    ball.setAttribute('r',PLAYER_RADIUS);
+    ball.setAttribute('cx', WIDTH / 4 * 3);
+    ball.setAttribute('cy', HEIGHT / 10);
+    ball.setAttribute('r', CIRCLE_RADIUS);
     ball.setAttribute('fill', '#F00');
     ball.speedX = 0;
     ball.speedY = 0;
     var pl1 = document.createElementNS(svgNS, 'circle');
     pl1.setAttribute('cx', WIDTH / 4);
-    pl1.setAttribute('cy', HEIGHT / (1+(1/20)));
-    pl1.setAttribute('r',PLAYER_RADIUS);
+    pl1.setAttribute('cy', HEIGHT / (1 + (1 / 20)));
+    pl1.setAttribute('r', CIRCLE_RADIUS);
     pl1.setAttribute('fill', '#0F0');
     pl1.speedX = 0;
     var pl2 = document.createElementNS(svgNS, 'circle');
     pl2.setAttribute('cx', WIDTH / 4 * 3);
     pl2.setAttribute('cy', HEIGHT / (1 + (1 / 20)));
-    pl2.setAttribute('r', PLAYER_RADIUS);
+    pl2.setAttribute('r', CIRCLE_RADIUS);
     pl2.setAttribute('fill', '#00F');
     pl2.speedX = 0;
     var net = document.createElementNS(svgNS, 'rect');
@@ -43,13 +44,13 @@
 
         switch (e.keyIdentifier) {
 
-            case  "Left":
+            case "Left":
                 pl2.movingLeft = true;
                 if (pl2.speedX <= 0) pl2.speedX = 1;
                 break;
-            case  "Right":
+            case "Right":
                 pl2.movingRight = true;
-                if (pl2.speedX <= 0) pl2.speedX=1;
+                if (pl2.speedX <= 0) pl2.speedX = 1;
                 break;
             default:
 
@@ -73,18 +74,23 @@
         }
     }
 
+
     function nextFrame() {
 
         var ballY = parseInt(ball.getAttribute('cy'));
+        var ballX = parseInt(ball.getAttribute('cx'));
+        ball.setAttribute('cx', ballX + ball.speedX);
         ball.setAttribute('cy', ballY + ball.speedY);
         ball.speedY = ball.speedY + G_ACCELERATION_FOR_FRAME;
-        console.log(ball.speedY);
+
+             checkForCollision();
+
 
         if (!pl2.movingLeft || !pl2.movingRight) {
 
             if (pl2.movingLeft) {
                 var pl2x = parseInt(pl2.getAttribute('cx'));
-                if (pl2x >= WIDTH / 2 + 2 + PLAYER_RADIUS + pl2.speedX) {
+                if (pl2x >= WIDTH / 2 + 2 + CIRCLE_RADIUS + pl2.speedX) {
                     pl2.setAttribute('cx', pl2x - pl2.speedX);
                     pl2.speedX = pl2.speedX + pl2.speedX * RUN_ACCELERATION_FOR_FRAME;
                     //console.log(pl2.speedX);
@@ -96,7 +102,7 @@
 
             if (pl2.movingRight) {
                 var pl2x = parseInt(pl2.getAttribute('cx'));
-                if (pl2x <= WIDTH - PLAYER_RADIUS - 1 - pl2.speedX) {
+                if (pl2x <= WIDTH - CIRCLE_RADIUS - 1 - pl2.speedX) {
                     pl2.setAttribute('cx', pl2x + pl2.speedX);
                     pl2.speedX = pl2.speedX + pl2.speedX * RUN_ACCELERATION_FOR_FRAME;
                 }
@@ -104,10 +110,38 @@
                     pl2.movingRight == false;
                 }
             }
+
+
+
+
+            requestAnimationFrame(nextFrame);
+        }
+    }
+
+    function checkForCollision() {
+        var ballY = parseInt(ball.getAttribute('cy'));
+        var ballX = parseInt(ball.getAttribute('cx'));
+        var pl2Y = parseInt(pl2.getAttribute('cy'));
+        var pl2X = parseInt(pl2.getAttribute('cx'));
+
+        // collision with pl2
+        if ((ballX - pl2X) * (ballX - pl2X) + (ballY - pl2Y) * (ballY - pl2Y) <= (CIRCLE_RADIUS * 2) * (CIRCLE_RADIUS * 2)) {
+            ball.speedY = -ball.speedY * КПД;
+            ball.speedX = (ballX - pl2X) * КПД;
+            return;
         }
 
+        if ((ballX - CIRCLE_RADIUS) <= 1){
+            ball.speedX = Math.abs(ball.speedX * КПД);
+        }
+         
+        if ((WIDTH - 1) <= (ballX + CIRCLE_RADIUS)) {
+            ball.speedX = -ball.speedX * КПД;
+        }
 
-        requestAnimationFrame(nextFrame);
+        if ((WIDTH / 2) >= (ballX - CIRCLE_RADIUS) && ball.speedX < 0 && ballY >= HEIGHT / 2 - CIRCLE_RADIUS) {
+            ball.speedX = -ball.speedX * КПД;
+        }
     }
 
     nextFrame();
