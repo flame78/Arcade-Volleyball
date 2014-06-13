24c7cfdab031588e5c8095b853e3852e2d9274d1
flame78 <flame78@gmail.com>
2014-06-13 20:59:21 +0300

    Extract "class Player" and make AI to JUMP

--- AV/scripts/Game.js
+++ AV/scripts/Game.js
@@ -1,91 +1,63 @@
-﻿"use strict"
+﻿/// <reference path="player.js" />
+var WIDTH = 320;
+var HEIGHT = 200;
+var КПД = 0.8;
+var CIRCLE_RADIUS = HEIGHT / 20;
+var PLAYERS_Y = HEIGHT / (1 + (1 / 20));
+var G_ACCELERATION_FOR_FRAME = 9.8 / 60;
+var RUN_ACCELERATION_FOR_FRAME = 9.8 / 60;
+var svgNS = 'http://www.w3.org/2000/svg';
 
 window.onload = function () {
-
-    function Player(element, radius, color, x, y, minLeft, maxRight) {
-        var svgNS = 'http://www.w3.org/2000/svg';
-        this.element = element;
-        this.element.setAttribute('r', CIRCLE_RADIUS);
-        this.element.setAttribute('fill', color);
-        this.element.setAttribute('cx', x);
-        this.element.setAttribute('cy', y);
-        this.element.setAttribute('r', CIRCLE_RADIUS);
-        this.x = x;
-        this.setX = function (x) {
-            this.x = x;
-            this.element.setAttribute('cx', x);
-        }
-        this.getX = this.x;
-        this.y = y; 
-        this.setY = function (y) {
-            this.y = y;
-            this.element.setAttribute('cy', y);
-        }
-        this.getY = this.y;
-        this.speedX = 0;
-        this.speedY = 0;
-        this.movingLeft = false;
-        this.movingRight = false;
-        this.jump = false;
-        this.jumpStopMove = false;
-        this.minLeft = minLeft;
-        this.maxRight = maxRight;
-
-        return this;
-    }
-
-    var WIDTH = 320;
-    var HEIGHT = 200;
-    var КПД = 0.8;
-    var CIRCLE_RADIUS = HEIGHT / 20;
-    var PLAYERS_Y = HEIGHT / (1 + (1 / 20));
-    var G_ACCELERATION_FOR_FRAME = 9.8 / 60;
-    var RUN_ACCELERATION_FOR_FRAME = 9.8 / 60;
-    var svgNS = 'http://www.w3.org/2000/svg';
 
     var svg = document.getElementById('the-svg');
 
     svg.setAttribute('width', WIDTH);
     svg.setAttribute('height', HEIGHT);
 
- /*   var stage = new Kinetic.Stage({
+    var stage = new Kinetic.Stage({
         container: 'container',
         width: WIDTH,
         height: WIDTH
     });
 
-    var layer = new Kinetic.Layer();
-
-    var imageObj = new Image();
-
-    imageObj.onload = function () {
-        var beach = new Kinetic.Image({
-            x: 0,
-            y: 0,
-            image: imageObj,
-            width: WIDTH,
-            height: HEIGHT
-        });
-
-        // add the shape to the layer
-        layer.add(beach);
-
-        // add the layer to the stage
-        stage.add(layer);
-    };
-
-    imageObj.src = 'images/beach.jpg';*/
-
-    document.body.style.backgroundImage.link('images/beach.jpg');
+    /*
+        var layer = new Kinetic.Layer();
+   
+       var imageObj = new Image();
+   
+       imageObj.onload = function () {
+           var beach = new Kinetic.Image({
+               x: 0,
+               y: 0,
+               image: imageObj,
+               width: WIDTH,
+               height: HEIGHT,
+               
+           });
+   
+           beach.setZIndex(10);
+   
+           // add the shape to the layer
+           layer.add(beach);
+   
+           // add the layer to the stage
+           stage.add(layer);
+       };
+   
+       imageObj.src = 'images/beach.jpg';
+   
+       document.body.style.backgroundImage.link('images/beach.jpg');
+       */
 
     var ball = document.getElementById('ball');
     var l3 = document.getElementById('Layer_3');
 
-    var pl1 = new Player(document.createElementNS(svgNS, 'circle'), CIRCLE_RADIUS, '#0F0', WIDTH / 4, PLAYERS_Y);
+    var pl1 = new Player(document.createElementNS(svgNS, 'circle'), CIRCLE_RADIUS, '#0F0', WIDTH / 4, PLAYERS_Y, CIRCLE_RADIUS, WIDTH / 2 - CIRCLE_RADIUS);
+    var pl2 = new Player(document.createElementNS(svgNS, 'circle'), CIRCLE_RADIUS, '#00F', WIDTH / 4 * 3, PLAYERS_Y, WIDTH / 2 + CIRCLE_RADIUS, WIDTH - CIRCLE_RADIUS);
 
-    var pl2 = document.createElementNS(svgNS, 'circle');
     var net = document.createElementNS(svgNS, 'rect');
-   
+
     net.setAttribute('x', WIDTH / 2 - 1);
     net.setAttribute('y', HEIGHT / 2);
     net.setAttribute('width', 2);
@@ -93,17 +65,13 @@ window.onload = function () {
     net.setAttribute('fill', '#888');
     ball.setAttribute('width', CIRCLE_RADIUS * 2);
     ball.setAttribute('height', CIRCLE_RADIUS * 2);
-    pl2.setAttribute('r', CIRCLE_RADIUS);
-    pl2.setAttribute('fill', '#00F');
 
     intializeGame();
 
     svg.appendChild(net);
     svg.appendChild(ball);
     svg.appendChild(pl1.element);
-    svg.appendChild(pl2);
-
-    console.dir(pl1);
+    svg.appendChild(pl2.element);
 
     nextFrame();
 
@@ -112,37 +80,27 @@ window.onload = function () {
         ball.setAttribute('y', HEIGHT / 10 - CIRCLE_RADIUS);
         ball.speedX = 0;
         ball.speedY = 0;
-        pl2.setAttribute('cx', WIDTH / 4 * 3);
-        pl2.setAttribute('cy', PLAYERS_Y);
-        pl2.speedX = 0;
-        pl2.speedY = 0;
-        pl2.movingLeft = false;
-        pl2.movingRight = false;
-        pl2.jump = false;
-        pl2.jumpStopMove = false;
     }
-
-
 
     window.onkeydown = function (e) {
 
-        console.dir(e.keyIdentifier);
+        console.dir(e.keyCode);
 
-        switch (e.keyIdentifier) {
+        switch (e.keyCode) {
 
-            case "Left":
-          //      if (!pl2.jump) {
-                    if (!pl2.movingLeft) pl2.speedX = -1;
-                    pl2.movingLeft = true;
-          //      }
+            case 37:
+                //      if (!pl2.jump) {
+                if (!pl2.movingLeft) pl2.speedX = -1;
+                pl2.movingLeft = true;
+                //      }
                 break;
-            case "Right":
-            //    if (!pl2.jump) {
-                    if (!pl2.movingRight) pl2.speedX = 1;
-                    pl2.movingRight = true;
-         //       }
+            case 39:
+                //    if (!pl2.jump) {
+                if (!pl2.movingRight) pl2.speedX = 1;
+                pl2.movingRight = true;
+                //       }
                 break;
-            case "Up":
+            case 38:
                 if (!pl2.jump) {
                     pl2.speedY = 4;
                     pl2.jump = true;
@@ -155,28 +113,20 @@ window.onload = function () {
 
     window.onkeyup = function (e) {
 
-        switch (e.keyIdentifier) {
+        switch (e.keyCode) {
 
-            case "Left":
+            case 37:
 
-           /*     if (pl2.jump) {
-                    pl2.jumpStopMove = true;
-                }
-                else {*/
-                    pl2.movingLeft = false;
-                    pl2.speedX = 0;
-           //     }
+                pl2.movingLeft = false;
+                pl2.speedX = 0;
                 break;
 
-            case "Right":
-       /*         if (pl2.jump) {
-                    pl2.jumpStopMove = true;
-                }
-                else {*/
-                    pl2.movingRight = false;
-                    pl2.speedX = 0;
-            //    }
+            case 39:
+      
+                pl2.movingRight = false;
+                pl2.speedX = 0;
                 break;
+
             default:
 
         }
@@ -185,133 +135,35 @@ window.onload = function () {
     function nextFrame() {
         var ballY = parseFloat(ball.getAttribute('y')) + CIRCLE_RADIUS;
         var ballX = parseFloat(ball.getAttribute('x')) + CIRCLE_RADIUS;
-        var pl1x = pl1.getX;
-        var pl2x = parseFloat(pl2.getAttribute('cx'));
-        var pl2y = parseFloat(pl2.getAttribute('cy'));
 
         ball.setAttribute('x', ballX - CIRCLE_RADIUS + ball.speedX);
         ball.setAttribute('y', ballY - CIRCLE_RADIUS + ball.speedY);
 
+        // rottate ball
         l3.setAttribute('transform', 'translate(-90,-90) scale(1.35) rotate(' + (ball.speedX * ball.speedY) * 20 + ' 250 250)');
+
         ball.speedY = ball.speedY + G_ACCELERATION_FOR_FRAME;
 
         checkForCollision();
 
         aI();
 
-        if (pl1.movingLeft) {
+        pl1.update();
+        pl2.update();
 
-            if (pl1x - CIRCLE_RADIUS * 2 >= 1 + pl2.speedX) {
-
-                pl1.setX(pl1x + pl1.speedX);
-
-       //         if (!pl1.jump) {
-
-                    pl1.speedX = pl1.speedX - RUN_ACCELERATION_FOR_FRAME;
-      //          }
-            }
-            else {
-                pl1.movingLeft = false;
-                pl1.speedX = 0;
-                pl1.setX( CIRCLE_RADIUS );
-
-            }
-        }
-
-        if (pl1.movingRight) {
-            if (pl1x <= WIDTH/2 - CIRCLE_RADIUS * 2 - 1 - pl1.speedX) {
-                pl1.setX( pl1x + pl1.speedX);
-       //         if (!pl1.jump) {
-                    pl1.speedX = pl1.speedX + RUN_ACCELERATION_FOR_FRAME;
-       //         }
-            }
-            else {
-                pl1.movingRight = false;
-                pl1.speedX = 0;
-                pl1.setX( WIDTH/2 - CIRCLE_RADIUS-1);
-
-            }
-        }
-    /*    if (pl1.movingLeft) {
-
-            if (pl1x >= 0 + CIRCLE_RADIUS + 1 + pl1.speedX) {
-                pl1.setAttribute('cx', pl1x - pl1.speedX);
-                //  pl1.setAttribute('cx', ballX+CIRCLE_RADIUS);
-                pl1.speedX = pl1.speedX + RUN_ACCELERATION_FOR_FRAME;
-            }
-        }
-
-       if (pl1.movingRight) {
-            if (pl1x <= WIDTH / 2 - 2 - CIRCLE_RADIUS - pl1.speedX) {
-                pl1.setAttribute('cx', pl1x + pl1.speedX);
-                //  pl1.setAttribute('cx', ballX + CIRCLE_RADIUS);
-                pl1.speedX = pl1.speedX + RUN_ACCELERATION_FOR_FRAME;
-            }
-        } */
-
-        if (pl2.jump) {
-
-            pl2.setAttribute('cy', pl2y - pl2.speedY);
-
-            console.log(pl2y);
-
-            pl2.speedY = pl2.speedY - RUN_ACCELERATION_FOR_FRAME;
-
-            if (pl2y >= PLAYERS_Y + 1) {
-         /*       if (pl2.jumpStopMove) {
-                    pl2.movingLeft = false;
-                    pl2.movingRight = false;
-                    pl2.speedX = 0;
-                }*/
-                pl2.jumpStopMove = false;
-                pl2.jump = false;
-                pl2.speedY = 0;
-                pl2.setAttribute('cy', PLAYERS_Y);
-            }
-
-        }
-
-        if (pl2.movingLeft) {
-
-            if (pl2x - CIRCLE_RADIUS*2 >= WIDTH / 2 + 3 +pl2.speedX) {
-
-                pl2.setAttribute('cx', pl2x + pl2.speedX);
-
-       //         if (!pl2.jump) {
-
-                    pl2.speedX = pl2.speedX - RUN_ACCELERATION_FOR_FRAME;
-       //         }
-            }
-            else {
-                pl2.movingLeft = false;
-                pl2.speedX = 0;
-                pl2.setAttribute('cx', WIDTH / 2 + CIRCLE_RADIUS + 1 );
-
-            }
-        }
-
-        if (pl2.movingRight) {
-            if (pl2x <= WIDTH - CIRCLE_RADIUS*2 - 1 - pl2.speedX) {
-                pl2.setAttribute('cx', pl2x + pl2.speedX);
-      //          if (!pl2.jump) {
-                    pl2.speedX = pl2.speedX + RUN_ACCELERATION_FOR_FRAME;
-      //          }
-            }
-            else {
-                pl2.movingRight = false;
-                pl2.speedX = 0;
-                pl2.setAttribute('cx', WIDTH - CIRCLE_RADIUS );
-
-            }
-        }
         requestAnimationFrame(nextFrame);
     }
 
     function aI() {
 
-        var pl1x = pl1.getX;
-        console.log(pl1x);
-        var ballX = parseFloat(ball.getAttribute('x')) - CIRCLE_RADIUS;
+        var pl1x = pl1.x;
+        var ballX = parseFloat(ball.getAttribute('x'));
+        var ballY = parseFloat(ball.getAttribute('y'));
+
+        if (!pl1.jump && ball.speedY < 10 && ballY<HEIGHT - CIRCLE_RADIUS*3 && ballY>HEIGHT/2 && ballX<WIDTH/2) {
+            pl1.speedY = 4;
+            pl1.jump = true;
+        }
 
         if (ballX < pl1x) {
             if (pl1.speedX == 0 || pl1.movingRight) pl1.speedX = -1;
@@ -329,29 +181,10 @@ window.onload = function () {
     function checkForCollision() {
         var ballY = parseFloat(ball.getAttribute('y')) + CIRCLE_RADIUS;
         var ballX = parseFloat(ball.getAttribute('x')) + CIRCLE_RADIUS;
-        var pl2Y = parseFloat(pl2.getAttribute('cy'));
-        var pl2X = parseFloat(pl2.getAttribute('cx'));
-        var pl1Y = pl1.getY;
-        var pl1X = pl1.getX;
-
-        // collision with pl1
-        if ((ballX - pl1X) * (ballX - pl1X) + (ballY - pl1Y) * (ballY - pl1Y) <= (CIRCLE_RADIUS * 2) * (CIRCLE_RADIUS * 2)) {
-            ball.speedY = -ball.speedY * КПД + pl1.speedY;
-            ball.speedX = (ballX - pl1X)/4 * КПД;
-            ball.setAttribute('y', pl1Y - CIRCLE_RADIUS * 3);
-            return;
-        }
-
-        // collision with pl2
-        if ((ballX - pl2X) * (ballX - pl2X) + (ballY - pl2Y) * (ballY - pl2Y) <= (CIRCLE_RADIUS * 2) * (CIRCLE_RADIUS * 2)) {
-            ball.speedY = -ball.speedY * КПД - pl2.speedY;
-            ball.speedX = (ballX - pl2X)/4 * КПД;
-            ball.setAttribute('y', pl2Y - CIRCLE_RADIUS * 3);
-        }
 
         // drop
         if ((ballY + CIRCLE_RADIUS) > HEIGHT) {
-          //  alert('lose');
+            //  alert('lose');
             // intializeGame();
             ball.speedY = -ball.speedY;
         }
@@ -373,9 +206,8 @@ window.onload = function () {
             ball.setAttribute('x', (WIDTH - CIRCLE_RADIUS * 2));
         }
 
-
         // net from right
-        if ((WIDTH / 2) >= (ballX - CIRCLE_RADIUS) && ballY >= HEIGHT / 2  && ballX>WIDTH/2){
+        if ((WIDTH / 2) >= (ballX - CIRCLE_RADIUS) && ballY >= HEIGHT / 2 && ballX > WIDTH / 2) {
             ball.speedX = -ball.speedX * КПД;
             ball.setAttribute('x', WIDTH / 2);
             ballX = WIDTH / 2 + CIRCLE_RADIUS;
@@ -387,9 +219,5 @@ window.onload = function () {
             ball.setAttribute('x', WIDTH / 2 - CIRCLE_RADIUS * 2);
             ballX = WIDTH / 2 - CIRCLE_RADIUS;
         }
-
-        //(WIDTH / 2) >= (ballX - CIRCLE_RADIUS) && 
     }
-
-
 }
